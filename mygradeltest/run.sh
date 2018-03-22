c2bac5e9d8f02f9f34a06e6d937b7e6ce9dac49f
aaray6 <aaray21cn@gmail.com>
2018-03-22 12:09:04 +0800

    update docker file folder

---
+++ mygradeltest/run.sh
@@ -0,0 +1,5 @@
+docker-compose stop mygradletest
+docker-compose rm -f mygradletest
+
+#docker-compose up --no-deps -d mygradletest
+docker-compose up --no-deps mygradletest
---
+++ mygradeltest/stop.sh
@@ -0,0 +1,2 @@
+docker-compose stop myspringboottest2
+docker-compose rm -f myspringboottest2
